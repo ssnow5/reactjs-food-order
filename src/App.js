c7bfc82d6711416a8d7e19e1f7a9bbf0625aff76
David Tan <ssnow5@yahoo.com>
2025-02-23 05:29:18 +0800

    added cart modal V2

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Meals from './components/Meals/Meals';
 import CartProvider from './store/CartProvider';
 import Cart from './components/Cart/Cart';
 
+// V2
+
 function App() {
   return (
     <CartProvider>
